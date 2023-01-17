1df3b54a3dd4bf234b20931024df323144996964
chanathipgeist <chanathip.l5139@mail.kmutt.ac.th>
2023-01-17 12:30:02 +0700

    geistProject

---
+++ 012Chanathip/Semester2/Module1/geist-project/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {ref} from 'vue'
+</script>
+ 
+<template>
+
+  <div class="w-screen h-screen bg-neutral-300">
+    <!-- <firstPage /> -->
+  <h1>HelloWorld</h1>
+  </div>
+
+  <button class="btn-secondary">click </button>
+  
+  </template>
+  
+   
+<style scoped>
+
+</style>
